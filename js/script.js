dfa8da6b3a8f69f4993fdb9a5d85f30b225a6ff4
Jorge Costa <jre29costa@gmail.com>
2026-02-23 11:55:23 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// fazer uma referencia a cada botão de navegação 
+const start = document.getElementById("avancar");
+const login = document.getElementsByTagName("input")[3];
+const setavoltar = document.getElementById("setavoltar");
+
+
+
+if(start!=null){
+    start.onclick =()=>{
+        window.location.href="login.html";
+}
+}
+
+
+if(login!=null){
+    login.onclick = ()=>{
+        window.location.href="registrar.html";
+    }       
+}
+
+if(setavoltar!=null){
+    setavoltar.onclick =()=>{
+        window.location.href="index.html";
+    }
+}
+
